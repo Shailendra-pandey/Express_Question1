edf986569eabece2287538244a4474b8d5bfd0ba
Shailendra-pandey <shailendrapandey028@gmail.com>
2022-11-01 19:15:14 +0530

    sequelizejs

--- models/useraddress.model.js
+++ models/useraddress.model.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, type) => {
-  return sequelize.define("User", {
-    user_id: { type: type.String, allowNull: true },
-    address: { type: type.String, allowNull: true },
-    city: { type: type.String, allowNull: true },
-    state: { type: type.String, allowNull: true },
-    pin_code: { type: type.String, allowNull: true },
-    phone_no: { type: type.String, allowNull: true }
+  return sequelize.define("Address", {
+    user_id: { type: type.INTEGER, allowNull: true },
+    address: { type: type.STRING, allowNull: true },
+    city: { type: type.STRING, allowNull: true },
+    state: { type: type.STRING, allowNull: true },
+    pin_code: { type: type.STRING, allowNull: true },
+    phone_no: { type: type.STRING, allowNull: true }
   });
 };
